6d2ee7492efe961ba04f68994105fa603b553a21
Nicolas Barray <nbarray@gmail.com>
2017-11-12 00:31:23 +0100

    add debug images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import styled from 'styled-components';
+import VanGogh from './img/paintings/van-gogh-0.jpg';
+import VanGogh1 from './img/paintings/van-gogh-1.jpg';
+import VanGogh2 from './img/paintings/van-gogh-2.jpg';
+
+const View = styled.div`
+  /* Dimensions */
+  height: 100vh;
+
+  /* Layout */
+  display: flex;
+  flex-flow: column nowrap;
+
+  align-items: center;
+`
+
+const Painting = styled.div`
+  width: 80%;
+  height: 80%;
+  margin: 32px;
+  
+  background-image: url(${props => props.src});
+  background-size: contain;
+  background-repeat: no-repeat;
+
+  /* Shadows */
+  box-shadow: 0px 16px 32px #888888;
+`
+console.log(VanGogh1)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View>
+        <Painting src={VanGogh} alt='van-gogh-0' />
+      </View>
     );
   }
 }
